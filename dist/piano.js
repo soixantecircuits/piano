0871c38f6e7e0eef2f764108a90f96f6cad9a392
Arthur Battut <arthur.battut@gmail.com>
2015-11-16 17:44:07 +0100

    add build task

---
+++ dist/piano.js
@@ -0,0 +1,304 @@
+/**
+ * Piano
+ * Desktop browser keyboard for touch screens
+ *
+ * @author hugohil <hugo@soixantecircuits.fr>
+ * @license MIT
+ * @version 1.0b
+ */
+
+var piano = (function (k){
+  'use strict';
+
+  var k = {};
+
+  k.init = function (options){
+    k.container = document.createElement('div');
+    k.container.id = 'piano';
+    k.container.className = 'piano-container animated';
+    console.log(options.triggers);
+    k.triggerName = options.triggers || 'click';
+
+    k.triggers = document.querySelectorAll('[data-piano]');
+    for (var i = 0; i < k.triggers.length; i++) {
+      createKeyboard(k, k.triggers[i]);
+    }
+    document.body.appendChild(k.container);
+
+    // Make sure to hide keyboard when clicking outside
+    addMultipleListeners(['click', 'touchdown'], document, function (event){
+      if(event.target.dataset.piano != '' && !k.container.contains(event.target)){
+        k.hideKeyboard();
+      }
+    });
+  };
+
+  function createKeyboard (parent, target){
+    var _k = clone(parent);
+    delete _k.triggers;
+    delete _k.layouts;
+    var datas = target.dataset;
+    var options = {};
+
+    _k.shift = false;
+
+    if(datas.pianoPosition){
+      if(datas.pianoPosition.indexOf('absolute') > -1){
+        options.position = datas.pianoPosition.replace(/absolute,\s/gi, '').split(',');
+      } else {
+        options.position = (datas.pianoPosition) ? datas.pianoPosition.split(',') : [];
+      }
+    } else {
+        console.warn('It seems you have incorrect values in your data-piano-position attribute on element: ', target);
+        options.position = [];
+    }
+    options.layout = datas.pianoLayout;
+    options.limit = datas.pianoLimit;
+    options.animationIn = datas.pianoAnimationIn;
+    options.animationOut = datas.pianoAnimationOut;
+    options.scale = datas.pianoScale;
+
+    var eventID = datas.pianoEventId;
+    var elementEvent = null;
+    if(eventID){
+      elementEvent = document.createEvent('Event');
+      elementEvent.initEvent(eventID, true, true);
+    }
+
+    _k.settings = {
+      position: {
+        x: options.position[0] || 'center',
+        y: options.position[1] || 'bottom'
+      },
+      layout: options.layout || 'default',
+      limit: options.limit || -1,
+      submitEvent: elementEvent,
+      animationIn: options.animationIn || 'fadeInUp',
+      animationOut: options.animationOut || 'fadeOutDown',
+      scale: options.scale || 1
+    }
+
+    addMultipleListeners(['click', 'touchdown'], target, function (event){
+      k.clearKeyboards();
+      k.currentTarget = event.target;
+      displayKeyboard(_k);
+    });
+  };
+
+  function displayKeyboard (instance){
+    var _k = k;
+    _k.currentKeyboard = instance;
+
+    var rowsContainer = document.createElement('div');
+    rowsContainer.className = 'piano-rows';
+
+    var layout = k.layouts[instance.settings.layout];
+
+    var rows = [];
+    rows.push(document.createElement('ul'));
+
+    for(var i in layout){
+      var li = document.createElement('li');
+      if(layout[i] == 'break'){
+        rowsContainer.appendChild(rows[rows.length - 1]);
+        rows.push(document.createElement('ul'));
+      } else {
+        var key = document.createElement('button');
+        if(typeof(layout[i][0]) == 'object'){
+          li.className = layout[i][0].name;
+          key.className = 'key ' + layout[i][0].name;
+          key.innerHTML = layout[i][0].value;
+          key.dataset.pianoKey = layout[i][0].name;
+        } else {
+          key.className = 'key ' + layout[i];
+          key.textContent = layout[i][0];
+          key.dataset.pianoKey = layout[i][0];
+        }
+        addMultipleListeners(_k.triggerName, key, function (event){
+          _k.debounce(keyPressed(event), 300, false);
+        });
+        li.appendChild(key);
+      }
+      rowsContainer.appendChild(rows[rows.length - 1]);
+
+      rows[rows.length - 1].appendChild(li);
+    }
+
+    if(isNaN(instance.settings.position.x) || isNaN(instance.settings.position.y)){
+      k.container.className += ' ' + instance.settings.position.x;
+      k.container.className += ' ' + instance.settings.position.y;
+    } else {
+      k.container.style.left = instance.settings.position.x + 'px';
+      k.container.style.top = instance.settings.position.y + 'px';
+    }
+
+    k.container.classList.remove(k.currentKeyboard.settings.animationOut);
+    k.container.appendChild(rowsContainer);
+    k.container.classList.add(k.currentKeyboard.settings.animationIn);
+
+    k.container.querySelector('.piano-rows').style['-webkit-transform'] = 'scale('+_k.currentKeyboard.settings.scale+')';
+    k.container.querySelector('.piano-rows').style['-moz-transform'] = 'scale('+_k.currentKeyboard.settings.scale+')';
+    k.container.querySelector('.piano-rows').style['-ms-transform'] = 'scale('+_k.currentKeyboard.settings.scale+')';
+    k.container.querySelector('.piano-rows').style['-o-transform'] = 'scale('+_k.currentKeyboard.settings.scale+')';
+    k.container.querySelector('.piano-rows').style['transform'] = 'scale('+_k.currentKeyboard.settings.scale+')';
+
+
+    document.body.classList.add('piano-open');
+  };
+
+  function keyPressed (event){
+    var target = event.target;
+    var value = target.textContent;
+    var input = k.currentTarget;
+    var cursor = input.selectionStart;
+    var end = input.selectionEnd;
+    var diff = (end - cursor) || 1;
+    var offset = 1;
+    var limit =  k.currentKeyboard.settings.limit;
+    var submitEvent = k.currentKeyboard.settings.submitEvent;
+
+    // There are still small bugs with selections.
+    if(/del/i.test(target.className)){
+      var deleteOffset = (cursor) ? -1 : 0;
+      input.value = insertToString(input.value, (cursor + deleteOffset), diff, '');
+      offset = -1;
+    } else if(/space/i.test(target.className)){
+      if(input.value.length <= limit || limit == -1){
+        input.value = insertToString(input.value, cursor, (diff - 1), ' ');
+      }
+    } else if(/shift/i.test(target.className)){
+      k.switchCase();
+    } else if(/larr/i.test(target.className)){
+      offset = -1;
+    } else if(/rarr/i.test(target.className)){
+      offset = 1;
+    } else if(/hide/i.test(target.className)){
+      k.hideKeyboard();
+    } else if(/submit/i.test(target.className)){
+      if(submitEvent){
+        input.dispatchEvent(submitEvent);
+      } else {
+        console.warn('You did not provide a data-piano-event-id attribute.')
+      }
+    } else {
+      if(input.value.length <= limit || limit == -1){
+        input.value = insertToString(input.value, cursor, (diff - 1), value);
+      }
+    }
+
+    input.selectionStart = cursor + offset;
+    input.selectionEnd = cursor + offset;
+
+    if (document.createEvent) {
+      var evt = document.createEvent('HTMLEvents');
+      evt.initEvent('input', false, true);
+      input.dispatchEvent(evt);
+    } else {
+      input.fireEvent('input');
+    }
+
+    input.focus();
+  };
+
+  k.switchCase = function (){
+    var shift = k.currentKeyboard.shift = !k.currentKeyboard.shift;
+    var keys = document.querySelectorAll('.piano-rows > ul > li');
+    var layout = k.layouts[k.currentKeyboard.settings.layout];
+
+    for (var i = 0; i < keys.length; i++) {
+      var target = keys[i].children[0];
+      if(target){
+        var value = target.textContent;
+        if(layout[i].length > 1 && layout[i][0].length > 0){
+          target.textContent = (shift) ? layout[i][1] : layout[i][0];
+        } else {
+          target.textContent = (shift) ? value.toUpperCase() : value.toLowerCase();
+        }
+      }
+    }
+  };
+
+  k.hideKeyboard = function (){
+    if(k.container.firstChild){
+      k.container.classList.remove(k.currentKeyboard.settings.animationIn);
+      k.container.classList.add(k.currentKeyboard.settings.animationOut);
+      document.body.classList.remove('piano-open');
+    }
+  };
+
+  k.clearKeyboards = function (){
+    if(k.container.firstChild){
+      k.container.firstChild.remove();
+      k.container.style.top = k.container.style.left = '';
+      k.container.className = 'piano-container animated';
+      k.currentKeyboard = null;
+    }
+  };
+
+  // Found on http://stackoverflow.com/a/21350614/2033455
+  function insertToString (str, index, count, add){
+    return str.slice(0, index) + (add || "") + str.slice(index + count);
+  };
+
+  function addMultipleListeners (events, target, handler){
+    events = (events instanceof Array) ? events : [events];
+    for (var i = 0; i < events.length; i++) {
+      target.addEventListener(events[i], function (event){
+        handler(event);
+      });
+    }
+  };
+
+  function clone (src){
+    var dest = {};
+    for (var key in src) {
+      dest[key] = src[key];
+    }
+    return dest;
+  };
+
+  // Helpers function for piano object
+  k.debounce = function(func, wait, immediate) {
+    var timeout;
+    return function() {
+      var context = this,
+        args = arguments;
+      var later = function() {
+        timeout = null;
+        if ( !immediate ) {
+          func.apply(context, args);
+        }
+      };
+      var callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait || 200);
+      if ( callNow ) { 
+        func.apply(context, args);
+      }
+    };
+  };
+
+  return k;
+})(piano || {});
+
+/**
+ * Piano
+ * Desktop browser keyboard for touch screens
+ *
+ * Default layout (QWERTY)
+ *
+ * @author hugohil <hugo@soixantecircuits.fr>
+ * @license MIT
+ * @version 1.0b
+ */
+
+(function (k){
+  k.layouts = k.layouts || [];
+  k.layouts['default'] = [
+    ['1', '!'], ['2', '@'], ['3', '#'], ['4', '$'], ['5', '%'], ['6', '^'], ['7', '&'], ['8', '*'], ['9', '('], ['0', ')'], ['-', '_'], ['=', '+'], [{name: 'del', value: '⌫'}], ['break'],
+    ['q'], ['w'], ['e'], ['r'], ['t'], ['y'], ['u'], ['i'], ['o'], ['p'], ['[', '{'], [']', '}'], ['\\', '|'], ['break'],
+    ['a'], ['s'], ['d'], ['f'], ['g'], ['h'], ['j'], ['k'], ['l'], [';', ':'], ['\'', '"'], ['break'],
+    [{name: 'shift', value: '↑'}], ['z'], ['x'], ['c'], ['v'], ['b'], ['n'], ['m'], [',', '<'], ['.', '>'], ['/', '?'], [{name: 'shift', value: '↑'}], ['break'],
+    [{name: 'larr', value: '<'}], [{name: 'space', value: ' '}], [{name: 'rarr', value: '>'}], [{name: 'hide', value: '⌨'}]
+  ];
+})(piano || {});
--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
 var gulp = require('gulp');
 var gls = require('gulp-live-server');
+var concat = require('gulp-concat');
+var minify = require('gulp-minify');
+var minifyCss = require('gulp-minify-css');
 
 gulp.task('serve', function() {
   var server = gls.static('./', 8080);
@@ -8,4 +11,22 @@ gulp.task('serve', function() {
   gulp.watch(['piano.*', 'layouts/**.*', 'demo/**.*'], function (file) {
     server.notify.apply(server, [file]);
   });
+});
+gulp.task('build', function(){
+  console.log('build lib');
+  gulp.src(['./piano.js', './layouts/default.js'])
+    .pipe(concat('piano.js'))
+    .pipe(gulp.dest('./dev/'));
+
+  gulp.src('./dev/*.js')
+    .pipe(minify({
+      exclude: ['tasks'],
+      ignoreFiles: ['.combo.js', '-min.js'],
+      mangle: false
+    }))
+    .pipe(gulp.dest('./dist/'));
+
+  gulp.src('./piano.css')
+    .pipe(minifyCss({compatibility: 'ie8'}))
+    .pipe(gulp.dest('./dist/'));
 });

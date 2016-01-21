c583f324963e3f5959549dc84da4ce7332cd7ee4
gabrielstuff <gabriel@soixantecircuits.fr>
2016-01-21 23:46:16 +0100

    first draft of ES6 conversion for Piano

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+### [0.0.1](https://github.com/soixantecircuits/piano/releases/tag/v0.0.1)
+
+- The first release
--- demo/main.js
+++ demo/main.js
@@ -1,25 +1,21 @@
-/* globals piano */
+keyboard = new piano({
+  triggers: ['click', 'touchstart'],
+  slideContent: true,
+  slideContainer: '.demo-container',
+  onHidden: function () {
+    console.log("hidden")
+  },
+  onBeforeHidden: function () {
+    console.log("hidding...")
+  }
+})
 
-;(function () {
-  piano.init({
-    triggers: ['click', 'touchstart'],
-    slideContent: true,
-    slideContainer: '.demo-container',
-    onHidden: function () {
-      console.log("hidden")
-    },
-    onBeforeHidden: function () {
-      console.log("hidding...")
-    }
-  })
+document.querySelector('textarea').click()
 
-  document.querySelector('textarea').click()
+document.querySelector('#input-2').addEventListener('input', function (event) {
+  console.log('input changed')
+})
 
-  document.querySelector('#input-2').addEventListener('input', function (event) {
-    console.log('input changed')
-  })
-
-  document.querySelector('#input-2').addEventListener('input-2', function (event) {
-    console.log('element with id "%s" submitted.', event.target.id)
-  })
-})()
+document.querySelector('#input-2').addEventListener('input-2', function (event) {
+  console.log('element with id "%s" submitted.', event.target.id)
+})

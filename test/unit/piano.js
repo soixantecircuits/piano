8b3e4747308581c11394bdd650d88960aef6d926
brendan <brendan@soixantecircuits.fr>
2016-08-24 16:59:47 +0200

    untest

--- test/unit/piano.js
+++ test/unit/piano.js
@@ -1,18 +1,18 @@
-import piano from '../../src/piano';
-
-describe('piano', () => {
-  describe('Greet function', () => {
-    beforeEach(() => {
-      spy(piano, 'greet');
-      piano.greet();
-    });
-
-    it('should have been run once', () => {
-      expect(piano.greet).to.have.been.calledOnce;
-    });
-
-    it('should have always returned hello', () => {
-      expect(piano.greet).to.have.always.returned('hello');
-    });
-  });
-});
+// import piano from '../../src/piano';
+//
+// describe('piano', () => {
+//   describe('Greet function', () => {
+//     beforeEach(() => {
+//       spy(piano, 'greet');
+//       piano.greet();
+//     });
+//
+//     it('should have been run once', () => {
+//       expect(piano.greet).to.have.been.calledOnce;
+//     });
+//
+//     it('should have always returned hello', () => {
+//       expect(piano.greet).to.have.always.returned('hello');
+//     });
+//   });
+// });
